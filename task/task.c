11c82837c4651b9870f9e7d6560a852391a640c5
ziyang22 <ziyang22@users.noreply.github.com>
2026-09-19 22:34:26 +0800

    Add two-dimensional dense thread grids

--- task/task.c
+++ task/task.c
@@ -11,8 +11,11 @@
 #define PROFILE_PRINT(...) ((void)0)
 #endif
 
-#ifndef SRQ_DENSE_THREADS
-#define SRQ_DENSE_THREADS 32
+#ifndef SRQ_DENSE_M_THREADS
+#define SRQ_DENSE_M_THREADS 32
+#endif
+#ifndef SRQ_DENSE_N_THREADS
+#define SRQ_DENSE_N_THREADS 1
 #endif
 
 extern void cblas_dgemm(const int order, const int trans_a, const int trans_b,
@@ -106,16 +109,20 @@ static void multiply_dense_blas(const double* A, const double* B, double* C,
     const double t_start = PROFILE_NOW();
     const int saved_threads = openblas_get_num_threads();
     openblas_set_num_threads(1);
-#pragma omp parallel num_threads(SRQ_DENSE_THREADS)
+#pragma omp parallel num_threads(SRQ_DENSE_M_THREADS * SRQ_DENSE_N_THREADS)
     {
         const int tid = omp_get_thread_num();
-        const int threads = omp_get_num_threads();
-        const int first = (int)((long long)M * tid / threads);
-        const int last = (int)((long long)M * (tid + 1) / threads);
-        if (last > first) {
-            cblas_dgemm(101, 111, 111, last - first, N, K,
-                        1.0, A + (size_t)first * K, K,
-                        B, N, 0.0, C + (size_t)first * N, N);
+        const int m_part = tid / SRQ_DENSE_N_THREADS;
+        const int n_part = tid % SRQ_DENSE_N_THREADS;
+        const int first_m = (int)((long long)M * m_part / SRQ_DENSE_M_THREADS);
+        const int last_m = (int)((long long)M * (m_part + 1) / SRQ_DENSE_M_THREADS);
+        const int first_n = (int)((long long)N * n_part / SRQ_DENSE_N_THREADS);
+        const int last_n = (int)((long long)N * (n_part + 1) / SRQ_DENSE_N_THREADS);
+        if (last_m > first_m && last_n > first_n) {
+            cblas_dgemm(101, 111, 111, last_m - first_m, last_n - first_n, K,
+                        1.0, A + (size_t)first_m * K, K,
+                        B + first_n, N, 0.0,
+                        C + (size_t)first_m * N + first_n, N);
         }
     }
     openblas_set_num_threads(saved_threads);
