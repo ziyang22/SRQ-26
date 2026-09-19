595d5e4e9272e498c604747f9d966e7cbe5aed32
ziyang22 <ziyang22@users.noreply.github.com>
2026-09-19 21:45:24 +0800

    Use the AVX-512 vector exponential ABI

--- task/activation.c
+++ task/activation.c
@@ -3,16 +3,41 @@
 #include <omp.h>
 
 #if defined(__GNUC__) && defined(__x86_64__)
-__attribute__((optimize("fast-math"), target("avx512f")))
-#endif
+typedef double vector8d __attribute__((vector_size(64), aligned(1)));
+extern vector8d _ZGVeN8v_exp(vector8d value);
+
+__attribute__((target("avx512f")))
 static void epilogue_chunk(double* data, size_t first, size_t last)
 {
-#pragma omp simd
+    const vector8d scale = { EPILOGUE_SCALE, EPILOGUE_SCALE,
+                             EPILOGUE_SCALE, EPILOGUE_SCALE,
+                             EPILOGUE_SCALE, EPILOGUE_SCALE,
+                             EPILOGUE_SCALE, EPILOGUE_SCALE };
+    const vector8d bias = { EPILOGUE_BIAS, EPILOGUE_BIAS,
+                            EPILOGUE_BIAS, EPILOGUE_BIAS,
+                            EPILOGUE_BIAS, EPILOGUE_BIAS,
+                            EPILOGUE_BIAS, EPILOGUE_BIAS };
+    const vector8d one = { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
+    size_t i = first;
+    for (; i + 8 <= last; i += 8) {
+        vector8d x = *(const vector8d*)(data + i) * scale + bias;
+        vector8d e = _ZGVeN8v_exp(-x);
+        *(vector8d*)(data + i) = one / (one + e);
+    }
+    for (; i < last; ++i) {
+        const double x = data[i] * EPILOGUE_SCALE + EPILOGUE_BIAS;
+        data[i] = 1.0 / (1.0 + exp(-x));
+    }
+}
+#else
+static void epilogue_chunk(double* data, size_t first, size_t last)
+{
     for (size_t i = first; i < last; ++i) {
         const double x = data[i] * EPILOGUE_SCALE + EPILOGUE_BIAS;
         data[i] = 1.0 / (1.0 + exp(-x));
     }
 }
+#endif
 
 /*
  * Tail processing chain: sigmoid(x * scale + bias).
