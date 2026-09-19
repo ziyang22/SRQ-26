47c2c9a1271f88140a47f48260c7ed4ca1948cb6
ziyang22 <ziyang22@users.noreply.github.com>
2026-09-19 21:42:01 +0800

    Apply fast math to the OpenMP epilogue worker

--- task/activation.c
+++ task/activation.c
@@ -1,6 +1,11 @@
 #include "../include/activation.h"
 #include <math.h>
 
+#if defined(__GNUC__)
+#pragma GCC push_options
+#pragma GCC optimize("fast-math")
+#endif
+
 /*
  * 尾处理链（epilogue）：epilogue(x) = sigmoid(x * scale + bias)
  */
@@ -12,3 +17,7 @@ void epilogue_optimized(double* data, size_t size)
         data[i] = 1.0 / (1.0 + exp(-x));
     }
 }
+
+#if defined(__GNUC__)
+#pragma GCC pop_options
+#endif
