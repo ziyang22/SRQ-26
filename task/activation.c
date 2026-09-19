afef50565e4173b5516be510c19a0843bef91d45
ziyang22 <ziyang22@users.noreply.github.com>
2026-09-19 21:42:43 +0800

    Vectorize sigmoid chunks with AVX-512 libmvec

--- task/activation.c
+++ task/activation.c
@@ -1,23 +1,30 @@
 #include "../include/activation.h"
 #include <math.h>
+#include <omp.h>
 
-#if defined(__GNUC__)
-#pragma GCC push_options
-#pragma GCC optimize("fast-math")
+#if defined(__GNUC__) && defined(__x86_64__)
+__attribute__((optimize("fast-math"), target("avx512f")))
 #endif
-
-/*
- * 尾处理链（epilogue）：epilogue(x) = sigmoid(x * scale + bias)
- */
-void epilogue_optimized(double* data, size_t size)
+static void epilogue_chunk(double* data, size_t first, size_t last)
 {
-#pragma omp parallel for schedule(static)
-    for (size_t i = 0; i < size; ++i) {
+#pragma omp simd
+    for (size_t i = first; i < last; ++i) {
         const double x = data[i] * EPILOGUE_SCALE + EPILOGUE_BIAS;
         data[i] = 1.0 / (1.0 + exp(-x));
     }
 }
 
-#if defined(__GNUC__)
-#pragma GCC pop_options
-#endif
+/*
+ * Tail processing chain: sigmoid(x * scale + bias).
+ */
+void epilogue_optimized(double* data, size_t size)
+{
+#pragma omp parallel
+    {
+        const size_t tid = (size_t)omp_get_thread_num();
+        const size_t threads = (size_t)omp_get_num_threads();
+        const size_t first = size * tid / threads;
+        const size_t last = size * (tid + 1) / threads;
+        epilogue_chunk(data, first, last);
+    }
+}
